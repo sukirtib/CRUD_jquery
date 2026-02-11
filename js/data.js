8a002cbf2caa74d49824709a6da1214bf9cee260
sukirtib <sukirtibasnet967@gmail.com>
2026-02-11 09:07:22 +0545

    fixing

--- js/data.js
+++ js/data.js
@@ -1,12 +1,4 @@
 
-var defaultGroceryItems = [
-    { id: "1", name: "milk", completed: true },
-    { id: "2", name: "bread", completed: true },
-    { id: "3", name: "eggs", completed: false },
-    { id: "4", name: "butter", completed: false },
-];
-
-
 function getInitialItems() {
     var storedItems = localStorage.getItem("grocery-list");
     if (storedItems) {
